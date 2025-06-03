a766d95dfd70b69daf622da37e37d82352f2e9e8
Isobelle Abson <21IAbson@heckgrammar.co.uk>
2025-06-03 21:51:32 +0100

    program

--- SpellCheckerTask/Program.cs
+++ SpellCheckerTask/Program.cs
@@ -15,7 +15,7 @@ namespace SpellCheckerTask
 
             //2. Take a string of words as a user input and check they have all been spelled correctly
 
-            //3.Create a spelling score based on the percentage of words spelled correctly
+            //3.Create a spelling score based on the percentage of words spelled correctly            
 
             //4.Create a new list of words that have been spelled incorrectly and save it in a new file
 
@@ -26,6 +26,57 @@ namespace SpellCheckerTask
 
             //Add these suggested words to a spelling list that the user can save as a file to work on
             //their own spelling
+
+            Console.WriteLine("Please write a word");
+            string[] UserWords = Console.ReadLine().Split(' ');
+            int correct = 0;
+            int wrong = 0;
+            //string path = "IncorrectWords.txt";
+
+            for (int i = 0; i < UserWords.Length; i++)
+            {
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if (UserWords[i].ToUpper() == words[j])
+                    {
+                        correct++;
+                        
+                    }
+                    else
+                    {
+                        wrong++;
+                        File.WriteAllText('C:\Users\isobe\Source\Repos\Heckgrammar\spell-checker-task-21IAbson\SpellCheckerTask\IncorrectWords.txt', UserWords[i]);     
+                    }
+                }
+
+            }
+            if (correct == 2)
+            {
+                Console.WriteLine("word is correct");
+            }
+            else
+            {
+                Console.WriteLine("word  is wrong");
+            }
+
+
+            //Percentage
+
+            double percentage = correct/UserWords.Length * 100;
+            Console.WriteLine(percentage + "%");
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
